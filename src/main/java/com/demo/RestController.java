ea855a08afba6aef46b6b18b06c2bfbc7c669937
tedro <tfinley@student.cccd.edu>
2020-04-08 18:02:52 -0700

    WebApplicationContext

--- src/main/java/com/demo/RestController.java
+++ src/main/java/com/demo/RestController.java
@@ -7,44 +7,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RestController {
 
     @Autowired
-//    private GreetInterface greeter1;
-    private Greeter greeter1;
+    private Greeter greeter;
 
     @Autowired
-//    private GreetInterface friendlyGreeter1;
-    private FriendlyGreeter friendlyGreeter1;
+    private FriendlyGreeter friendlyGreeter;
 
     @Autowired
-//    private GreetInterface greeter2;
-    private Greeter greeter2;
+    private GreetInterface greetAgain;
 
-    @Autowired
-//    private GreetInterface friendlyGreeter2;
-    private FriendlyGreeter friendlyGreeter2;
-
-    @Autowired
-    private GreetAgain greetagain;
-
-    @Autowired(required=false)
+    @Autowired(required = false)
     private NotActuallyABean notABean;
 
-    @RequestMapping("/hello1")
-    public String hello1() {
-        return greeter1.greet();
+    @RequestMapping("/hello")
+    public String hello() {
+        return greeter.greet();
     }
 
-    @RequestMapping("/hellofriendly1")
-    public String helloFriendly1() { return friendlyGreeter1.greet(); }
-
-    @RequestMapping("/hello2")
-    public String hello2() {
-        return greeter2.greet();
+    @RequestMapping("/hellofriendly")
+    public String helloFriendly() {
+        return friendlyGreeter.greet();
     }
-
-    @RequestMapping("/hellofriendly2")
-    public String helloFriendly2() { return friendlyGreeter2.greet(); }
 
     @RequestMapping("/helloagain")
-    public String helloAgain() { return greetagain.greet();
-    }
+    public String helloAgain() { return greetAgain.greet(); }
+
 }
