6b623484004821c6af71dbaaf1c94621c8a87a39
tmcfin <tfinley@student.cccd.edu>
2020-04-10 12:10:05 -0700

    CustomerController

--- src/main/java/com/demo/CustomerController.java
+++ src/main/java/com/demo/CustomerController.java
@@ -1,6 +1,9 @@
 package com.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +12,8 @@ public class CustomerController {
     @Autowired
     private CustomerRepository repository;
 
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    public Customer save(@RequestBody Customer customer) {
+        return repository.save(customer);
+    }
 }
