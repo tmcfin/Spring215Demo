370ee6debaeb6498cd3f03c4bf40f71ea71e7467
tmcfin <tfinley@student.cccd.edu>
2020-04-11 13:30:22 -0700

    CustomerController add endpoints get, post, put, delete

--- src/main/java/com/demo/CustomerController.java
+++ src/main/java/com/demo/CustomerController.java
@@ -1,10 +1,7 @@
 package com.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CustomerController {
@@ -15,5 +12,20 @@ public class CustomerController {
     @RequestMapping(value = "/customer", method = RequestMethod.POST)
     public Customer save(@RequestBody Customer customer) {
         return repository.save(customer);
+    }
+
+    @RequestMapping(value = "/customer", method = RequestMethod.PUT)
+    public Customer update(@RequestBody Customer customer) {
+        return repository.save(customer);
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
+    public Customer get(@PathVariable Long id) {
+        return repository.findById(id).orElse( null);
+    }
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        repository.deleteById(id);
     }
 }
